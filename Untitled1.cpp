6bdcc63e91f9ed153d848c4ea04ef9a320c1ff6b
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2017-11-08 10:25:16 +0800

    no1

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,5 +5,29 @@
 using namespace std;
 
 int main () {
-
+	int x,y,z;
+	cout <<"input nilai x=";
+	cin >> x;
+	
+	cout << "input nilai y = ";
+	cin >> y;
+	
+	
+	if (x>y) {
+		z = y-x;
+		cout<<abs(z)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
+	else if (y>x) {
+		z=x-y;
+		cout<<abs(z)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
+	else if (z<0) {
+		z=z*(-1);
+		cout<<abs (x-y)<<endl;
+		cout<<"Hasil Positif";
+	}
 }
